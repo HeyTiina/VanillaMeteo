463237273135deb025479d5d9734f663ab5a1439
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-02-17 17:21:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+async function search(event) {
+  event.preventDefault();
+
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  let tempElement = document.querySelector(".current-temperature-value");
+  let humidityElement = document.querySelector("#humidity-value");
+  let windSpeedElement = document.querySelector("#wind-speed");
+
+  let apiKey = "2a830c1f5845c71a9b8c68a49820t94o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=${apiKey}&units=metric`;
+
+  try {
+    let response = await axios.get(apiUrl);
+
+    cityElement.innerHTML = response.data.city;
+    tempElement.innerHTML = Math.round(response.data.temperature.current);
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    windSpeedElement.innerHTML = response.data.wind.speed;
+
+    console.log(response.data.temperature.current);
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+  }
+}
+
+document.querySelector("#search-form").addEventListener("submit", search);
+
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
