17b0d653172af6e2bc99572e5ad8ae167d49e292
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-02-19 19:04:06 +0100

    new Api for Forecast

--- src/index.js
+++ src/index.js
@@ -10,17 +10,18 @@ async function search(event) {
   let conditionElement = document.querySelector("#current-condition");
 
   let apiKey = "2a830c1f5845c71a9b8c68a49820t94o";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=${apiKey}&units=metric`;
+  let tempApiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=${apiKey}&units=metric`;
+  let forecastApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${searchInputElement.value}&key=${apiKey}&units=metric`;
 
   try {
-    let response = await axios.get(apiUrl);
+    let response = await axios.get(tempApiUrl);
 
     cityElement.innerHTML = response.data.city;
     tempElement.innerHTML = Math.round(response.data.temperature.current);
     humidityElement.innerHTML = response.data.temperature.humidity;
     windSpeedElement.innerHTML = response.data.wind.speed;
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-conditionElement.innerHTML = ` ${response.data.condition.description}`;
+    conditionElement.innerHTML = ` ${response.data.condition.description}`;
   } catch (error) {
     console.error("Error fetching weather data:", error);
   }
